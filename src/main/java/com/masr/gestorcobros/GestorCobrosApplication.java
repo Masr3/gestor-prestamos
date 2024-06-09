5ee679e19fd8f12bb70a355dd2853f92aab6e5fd
masr3 <manuelsantanar03@gmail.com>
2024-06-09 18:55:40 -0400

    Initializing project

---
+++ src/main/java/com/masr/gestorcobros/GestorCobrosApplication.java
@@ -0,0 +1,13 @@
+package com.masr.gestorcobros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorCobrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorCobrosApplication.class, args);
+    }
+
+}
